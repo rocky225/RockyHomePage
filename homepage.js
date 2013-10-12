e035e98357cac9df74061f4efb0e02fb95c3c1c0
rocky225 <rocky225@foxmail.com>
2013-10-12 14:38:52 +0800

    modify

--- homepage.js
+++ homepage.js
@@ -1,70 +1,33 @@
-var loadImage = function($scope, Projects){
-	$scope.projects = Projects;
-	setTimeout('call_quake()',1000);
-}
-
-var call_quake = function() {
-	$('.quake-slider').quake({
-		thumbnails: true,
-		animationSpeed: 500,
-		applyEffectsRandomly: true,
-		navPlacement: 'inside',
-		navAlwaysVisible: true,
-		captionOpacity: '0.3',
-		captionsSetup: [
-						 {
-							 "orientation": "top",
-							 "slides": [0, 1],
-							 "callback": captionAnimateCallback
-						 },
-						  {
-							  "orientation": "left",
-							  "slides": [2, 3],
-							  "callback": captionAnimationCallback1
-						  }
-						  ,
-						  {
-							  "orientation": "bottom",
-							  "slides": [4, 5],
-							  "callback": captionAnimateCallback
-						  }
-						  ,
-						  {
-							  "orientation": "right",
-							  "slides": [6, 7],
-							  "callback": captionAnimationCallback1
-						  }
-						]
-
-	});
-};
-
-function captionAnimateCallback(captionWrapper, captionContainer, orientation) {
-	captionWrapper.css({ left: '-990px' }).stop(true, true).animate({ left: 0 }, 500);
-	captionContainer.css({ left: '-990px' }).stop(true, true).animate({ left: 0 }, 500);
-}
-function captionAnimationCallback1(captionWrapper, captionContainer, orientation) {
-	captionWrapper.css({ top: '-330px' }).stop(true, true).animate({ top: 0 }, 500);
-	captionContainer.css({ top: '-330px' }).stop(true, true).animate({ top: 0 }, 500);
-}
-
-
-angular.module("mainApp",[]).
-  factory('Projects', function() {
-    return [{name:'Lanterns',src:'images/lanterns.jpg'},{name:'Tree',src:'images/tree.jpg'},
+var album_source = [{name:'Lanterns',src:'images/lanterns.jpg'},{name:'Tree',src:'images/tree.jpg'},
 			{name:'Cable Car',src:'images/cablecar.jpg'},{name:'London Eye',src:'images/londoneye.jpg'},
 			{name:'Golden Statue',src:'images/statue.jpg'},{name:'Under the Bridge',src:'images/bridge.jpg'},
 			{name:'Pregnant Maiden',src:'images/maiden.jpg'},{name:'Clouds of Doom',src:'images/clouds.jpg'},
 			{name:'Silver Statue',src:'images/statue2.jpg'}];
+
+$(function(){
+	$().maps();
+});			
+			
+angular.module("mainApp",[]).
+  factory('Projects', function() {
+    return album_source;
   }).config(function($routeProvider) {
-	$routeProvider.when('/',{templateUrl:'home.html'}).
-	when('/album',{controller:loadImage,templateUrl:'album.html'}).
+	$routeProvider.when('/',{controller:load_home_page,templateUrl:'home.html'}).
+	when('/album',{controller:load_album_page,templateUrl:'album.html'}).
 	otherwise({redirectTo:'/'});
 });
 
+var load_album_page = function($scope, Projects) {
+	$scope.projects = Projects;
+	archive("album_head");
+}
 
-var archive = function(it){
+var load_home_page = function() {
+	archive("home_head");
+}
+
+var archive = function(id){
 	$(".lionchange").removeClass("active");
-	$(it).addClass("active");
+	$("." + id).addClass("active");
 };
 
