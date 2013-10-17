5dac6681664ee4606c21c13b4d59c40427f5a942
rocky225 <rocky225@foxmail.com>
2013-10-17 17:10:58 +0800

    home
    instead of  metro style

--- homepage.js
+++ homepage.js
@@ -22,34 +22,27 @@ var load_album_page = function($scope, Projects) {
 	archive("album_head");
 }
 
+var home_page_divs = new Array();
 var load_home_page = function() {
+	var div_w = 260;
 	archive("home_head");
-	// Camera 
-/*
-	jQuery('.camera_wrap').camera({
-		height: '350px',
-//		loader: 'bar',
-		pagination: false,
-		thumbnails: true
-	});
-	*/
 	var home = $("#main_home_div");
-	var width = home.width() - 10;
-	var matrix_X = Math.floor(width/160);
-	home.width(matrix_X * 160);
-	var num = matrix_X * 5;
-	var matrix = [];
-	for(var index = 0;index < num;index++) {
-		if(!matrix[index]) {
-			var matrix_num = (Math.floor(Math.random() * 200))%14;
-			if(matrix_num == 8) {
-				matrix[index + matrix_X] = 1;
-			} else if(matrix_num > 8) {
-				index++;
-			}
-			home.append(metros[matrix_num]);
-		}
-	} 
+	var width = home.width();
+	var num_x = Math.floor(width/div_w);
+	home.width(num_x * div_w);
+	for(var i = 0;i<num_x;i++){
+		home.append("<div class='home_div_list'></div>");
+	}
+	home_page_divs = home[0].children;
+	for(var i=0;i<30;i++){
+		var r = Math.floor(Math.random()*255);
+		var g = Math.floor(Math.random()*255);
+		var b = Math.floor(Math.random()*255);
+		var h = 300 + Math.floor(Math.random()* 100);
+		var index = i % num_x;
+		$(home_page_divs[index]).append("<div class='home_div' style='height:" + h + "px;'><div>");
+//		home.append("<div class='home_div_list'></div>");
+	}
 }
 
 var archive = function(id){
@@ -57,20 +50,3 @@ var archive = function(id){
 	$("." + id).addClass("active");
 };
 
-var metros = [
-	"<div class=\"tile image\"><div class=\"tile-content\"><img src=\"images/metro_/myface.jpg\" alt=\"\"></div></div>",
-	"<div class=\"tile icon\"> <div class=\"tile-content\"> <img src=\"images/metro_/Mail128.png\"> </div> <div class=\"brand\"> <div class=\"badge\">10</div> <div class=\"name\">Mail</div> </div> </div>",
-	"<div class=\"tile bg-color-orangeDark icon selected\"> <b class=\"check\"></b> <div class=\"tile-content\"> <img src=\"images/metro_/Video128.png\" alt=\"\"> </div> <div class=\"brand\"> <span class=\"name\">Video</span> </div> </div>",
-	"<div class=\"tile bg-color-green icon selected\"> <b class=\"check\"></b> <div class=\"tile-content\"> <img src=\"images/metro_/Market128.png\"> </div> <div class=\"brand\"> <span class=\"name\">Store</span> <span class=\"badge\">6</span> </div> </div>",
-	"<div class=\"tile bg-color-red icon selected\"> <div class=\"tile-content\"> <img src=\"images/metro_/Music128.png\" alt=\"\"> </div> <div class=\"brand\"> <span class=\"name\">Music</span> </div> </div>",
-	"<div class=\"tile bg-color-blue icon\"> <div class=\"tile-content\"> <img src=\"images/metro_/InternetExplorer128.png\"> </div> <div class=\"brand\"> <span class=\"name\">Internet Explorer</span> </div> </div>",
-	"<div class=\"tile bg-color-darken icon\"> <div class=\"tile-content\"> <img src=\"images/metro_/YouTube128.png\" alt=\"\"> </div> <div class=\"brand\"> <span class=\"name\">YouTube</span> </div> </div>",
-	"<div class=\"tile icon\"> <div class=\"tile-content\"> <img src=\"images/metro_/excel2013icon.png\"> </div> <div class=\"brand\"> <span class=\"name\">Excel 2013</span> </div> </div>",
-	"<div class=\"tile double-vertical bg-color-yellow icon\"> <div class=\"tile-content\"> <img src=\"images/metro_/Calendar128.png\"> </div> <div class=\"brand\"> <span class=\"name\">Calendar</span> </div> </div>",
-	"<div class=\"tile double image\"> <div class=\"tile-content\"> <img src=\"images/metro_/5.jpg\" alt=\"\"> </div> <div class=\"brand\"> <span class=\"name\">Pictures</span> <div class=\"badge bg-color-orange\">5</div> </div> </div>",
-	"<div class=\"tile double bg-color-blueDark\"> <div class=\"tile-content\"> <img src=\"images/metro_/michael.jpg\" class=\"place-left\"> <h3 style=\"margin-bottom: 5px;\">michael_angiulo</h3> <p> I just saw Thor last night. It was awesome! I think you'd love it. </p><h5>RT @julie_green</h5> <p></p> </div> <div class=\"brand\"> <span class=\"name\">Tweet@rama</span> </div> </div>",
-	"<div class=\"tile double bg-color-green\"> <div class=\"tile-content\"> <h2>mattberg@live.com</h2> <h5>Re: Wedding Annoucement!</h5> <p> Congratulations! I'm really excited to celebrate with you all. Thanks for... </p> </div> <div class=\"brand\"> <div class=\"badge\">12</div> <img class=\"icon\" src=\"images/metro_/Mail128.png\"> </div> </div>",
-	"<div class=\"tile double image\"> <div class=\"tile-content\"> <img src=\"images/metro_/4.jpg\" alt=\"\"> </div> <div class=\"brand\">  <p class=\"text\" style=\"padding-bottom: 5px;\">This is a desert eagle. He is very hungry and angry bird.</p> <div class=\"badge\">10</div> </div> </div>",
-	"<div class=\"tile double image-set\"> <div class=\"tile-content\"> <img src=\"images/metro_/1.jpg\" alt=\"\"> </div> <div class=\"tile-content\"> <img src=\"images/metro_/2.jpg\" alt=\"\"> </div> <div class=\"tile-content\"> <img src=\"images/metro_/3.jpg\" alt=\"\"> </div> <div class=\"tile-content\"> <img src=\"images/metro_/4.jpg\" alt=\"\"> </div> <div class=\"tile-content\"> <img src=\"images/metro_/5.jpg\" alt=\"\"> </div> <div class=\"brand\"> <span class=\"name\">Photos</span> </div> </div>",
-	
-];
